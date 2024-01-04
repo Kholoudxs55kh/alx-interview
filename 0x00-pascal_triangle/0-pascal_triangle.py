e760e27e328d4975f612e54f765e434695084b1c
kholoudxs55kh <kfattem@gmail.com>
2024-01-04 08:08:28 +0200

    requirement edit

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,6 +2,7 @@
 """Defines a function that returns a list of lists of integers
 representing the Pascal’s triangle of n"""
 
+
 def pascal_triangle(n):
     """Returns a list of lists of integers representing the Pascal’s triangle of n"""
     if n <= 0:
