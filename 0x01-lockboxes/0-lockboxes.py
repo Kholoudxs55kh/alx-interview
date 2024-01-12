2c6b2e4d5291a78e0da394bdfb867e0d965063ae
kholoudxs55kh <kfattem@gmail.com>
2024-01-12 02:51:28 +0200

    test, should work

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+method that determines if all the boxes can be opened
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    determines if all the boxes can be opened
+    """
+    if not boxes:
+        return False
+
+    opened, checked = {0}, [0]
+
+    for currBox in checked:
+        opened.add(currBox)
+
+        for key in boxes[currBox]:
+            if key not in opened and key < len(boxes):
+                checked.append(key)
+
+    return len(opened) == len(boxes)
+
+# boxes = [[1], [2], [3], [4], []]
+# print(canUnlockAll(boxes))
+# True
+
+# boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+# print(canUnlockAll(boxes))
+# True
+
+# boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+# print(canUnlockAll(boxes))
+# False
