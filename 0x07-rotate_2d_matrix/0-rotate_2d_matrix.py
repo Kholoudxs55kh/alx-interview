0924ab622918463fa7a95692cfede8d9005302ac
Kholoud Fattem <kfattem@gmail.com>
2024-02-22 17:43:17 +0200

    add

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""_summary_
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """_summary_
+
+    Args:
+        matrix (_type_): _description_
+    """
+    n = len(matrix)
+    for i in range(n // 2):
+        for j in range(i, n - i - 1):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[n - j - 1][i]
+            matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1]
+            matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1]
+            matrix[j][n - i - 1] = temp
+    return matrix
